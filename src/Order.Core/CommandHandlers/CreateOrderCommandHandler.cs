cb0cfab1b8e9c9cc0c5df529a5301a7269597a4f
thuqnguyen <nqthu1012@gmail.com>
2020-10-14 13:46:52 +0700

    order call inventory and dont wait for response

--- src/Order.Core/CommandHandlers/CreateOrderCommandHandler.cs
+++ src/Order.Core/CommandHandlers/CreateOrderCommandHandler.cs
@@ -21,26 +21,22 @@ namespace Order.Core.CommandHandlers
 
         protected override Guid Handle(CreateOrderCommand command)
         {
-            var decreaseProductQuantityResult = stockClient.DecreaseProductQuantity(command.ProductId, 1)
-                .GetAwaiter()
-                .GetResult();
+            var order = Entities.Order.Create();
 
-            if (decreaseProductQuantityResult)
-            {
-                var order = Entities.Order.Create();
+            order.Id = Guid.NewGuid();
+            order.Start();
+            order.UpdateProductId(command.ProductId);
+            order.UpdateStatus(Entities.OrderStatus.New);
 
-                order.Id = Guid.NewGuid();
-                order.Start();
-                order.UpdateProductId(command.ProductId);
-                order.UpdateStatus(Entities.OrderStatus.New);
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            stockClient.DecreaseProductQuantity(command.ProductId, 1);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
-                bus.Publish(order.GetEvents(), command.Header);
+            bus.Publish(order.GetEvents(), command.Header);
 
-                repository.AddAsync(order);
+            repository.AddAsync(order);
 
-                return order.Id;
-            }
-            throw new Exception("Cannot create order.");
+            return order.Id;
         }
     }
 }
